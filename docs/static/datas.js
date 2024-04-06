cf6b046e9151f3d562b7d52e45f61eab8c7cc98a
suhailvs <suhailvs@gmail.com>
2024-04-06 10:23:16 +0530

    fixed an image

---
+++ docs/static/datas.js
@@ -0,0 +1,7 @@
+var RECTANGLES = [
+    { x: 1.321, y: -0.101, width: 0.03, height: 0.01, paleo: "ciaf", trans: "and~ MAN", l:"25:29:1"},
+    { x: 1.302, y: -0.1008, width: 0.014, height: 0.01, paleo: "ik", trans: "GIVEN THAT", l:"25:29:2" },
+    { x: 1.269, y:-0.1, width:0.031, height: 0.01, paleo:"rkmi", trans:"", l: "25:29:3"},
+    { x: 1.244, y:-0.098, width:0.021, height: 0.0094, paleo:"tib", trans:"", l: "25:29:4"},
+    { x: 1.208, y:-0.098, width:0.035, height: 0.0114, paleo:"bcfm", trans:"", l: "25:29:5"},
+  ];
--- docs/static/get_content.js
+++ docs/static/get_content.js
@@ -1,7 +1,4 @@
-var RECTANGLES = [
-  { x: 1.321, y: -0.101, width: 0.03, height: 0.01, paleo: "ciaf", trans: "and~ MAN", l:"25:29:1"},
-  { x: 1.302, y: -0.1008, width: 0.014, height: 0.01, paleo: "ik", trans: "GIVEN THAT", l:"25:29:2" },
-];
+
 function isPointInsideRectangle(x, y, rect) {
   return (
     x >= rect.x &&
