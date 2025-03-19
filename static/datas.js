641cd92fed6ecb7a9209f3cb9997de97cb09ba9f
suhailvs <suhailvs@gmail.com>
2025-03-19 11:26:32 +0530

    added more words

--- static/datas.js
+++ static/datas.js
@@ -1,6 +1,19 @@
 var RECTANGLES = [
-    { x: 1.321, y: -0.101, width: 0.03, height: 0.01, paleo: "ciaf", trans: "and~ MAN", l:"25:29:1"},
-    { x: 1.302, y: -0.1008, width: 0.014, height: 0.01, paleo: "ik", trans: "GIVEN THAT", l:"25:29:2" },
-    { x: 1.269, y:-0.1, width:0.031, height: 0.01, paleo:"rkmi", trans:"", l: "25:29:3"},
-    
-  ];
+  { x: 1.321, y: -0.101, width: 0.03, height: 0.01, paleo: "ciaf", trans: "and~ MAN", l:"25:29:1"},
+  { x: 1.302, y: -0.1008, width: 0.014, height: 0.01, paleo: "ik", trans: "GIVEN THAT", l:"25:29:2" },
+  { x: 1.269, y:-0.1, width:0.031, height: 0.01, paleo:"rkmi", trans:"", l: "25:29:3"},
+  { x: 1.244, y:-0.098, width:0.021, height: 0.0094, paleo:"tib", trans:"", l: "25:29:4"},
+  { x: 1.208, y:-0.098, width:0.035, height: 0.0114, paleo:"bcfm", trans:"", l: "25:29:5"},
+  { x: 1.183, y:-0.095, width:0.024, height: 0.0112, paleo:"rio", trans:"", l: "25:29:6"},
+  { x: 1.15, y:-0.091, width:0.03, height: 0.008, paleo:"emfh", trans:"", l: "25:29:7"},
+  { x: 1.11, y:-0.091, width:0.037, height: 0.011, paleo:"etief", trans:"", l: "25:29:8"},
+  { x: 1.07, y:-0.09, width:0.04, height: 0.014, paleo:"ftlag", trans:"", l: "25:29:9"},
+  { x: 1.05, y:-0.087, width:0.024, height: 0.011, paleo:"do", trans:"", l: "25:29:10"},
+  { x: 1.037, y:-0.088, width:0.013, height: 0.012, paleo:"mt", trans:"", l: "25:29:11"},
+  { x: 1.016, y:-0.087, width:0.023, height: 0.013, paleo:"tnc", trans:"", l: "25:29:12"},
+  { x: 0.98, y:-0.087, width:0.03, height: 0.014, paleo:"-", trans:"", l: "25:29:13"},
+  { x: 0.943, y:-0.087, width:0.035, height: 0.015, paleo:"-", trans:"", l: "25:29:14"},
+  { x: 1.32, y:-0.122, width:0.03, height: 0.011, paleo:"eiet", trans:"", l: "25:29:15"},
+  { x: 1.28, y:-0.12, width:0.04, height: 0.02, paleo:"ftlag", trans:"", l: "25:29:16"},
+  
+];
